c1cf03bdc6e711ab6e6c35814e0dbf7f7f7a6c29
BADIX <bogdan.aly@bk.ru>
2024-09-09 10:12:26 +0400

    2 PhotoImage
    123

--- badix52/badix52/Form1.cs
+++ badix52/badix52/Form1.cs
@@ -26,6 +26,8 @@ namespace badix52
         {
             pictureBox1.Image = Image.FromFile("\\\\192.168.3.250\\Veda\\3 курс\\ИСП 31-9\\МДК 03.02\\Алфёров Байков\\badix52\\badix52\\Без имени.png");
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.Image = Image.FromFile("\\\\192.168.3.250\\Veda\\3 курс\\ИСП 31-9\\МДК 03.02\\Алфёров Байков\\badix52\\badix52\\Без им52ени.png");
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
         }
     }
 }
